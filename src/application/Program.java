a41e8abd225bf5db56232e17e1714a1162cd35c6
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-06-10 14:50:17 -0300

    Organização do código para uma melhor leitura

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -21,10 +20,7 @@ public class Program {
 		
 		System.out.print("Enter full file path: ");
 		String path = sc.nextLine();
-		
-		System.out.print("Enter Salary: ");
-		Double salary = sc.nextDouble();
-		
+				
 		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			List<Employee> list = new ArrayList<>();
@@ -36,21 +32,24 @@ public class Program {
 				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
 				line = br.readLine();
 			}
+		
+			System.out.print("Enter Salary: ");
+			Double salary = sc.nextDouble();
 			
 			System.out.printf("Email of people whose salary is more than %.2f: %n", salary);
 			
-			Comparator<String> comp = (e1, e2) -> e1.toUpperCase().compareTo(e2.toUpperCase());
-			
 			List<String> emails = list.stream()
-									.filter(e -> e.getSalary() >= salary)
-									.map(e -> e.getEmail())
-									.sorted(comp)
-									.collect(Collectors.toList());
-			double sum = list.stream()
-							.filter(e -> e.getName().charAt(0) == 'M')
-							.map(e -> e.getSalary()).reduce(0.0, (x, y) -> x + y);
+					.filter(e -> e.getSalary() >= salary)
+					.map(e -> e.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
 			
 			emails.forEach(System.out::println);
+			
+			double sum = list.stream()
+					.filter(e -> e.getName().charAt(0) == 'M')
+					.map(e -> e.getSalary())
+					.reduce(0.0, (x, y) -> x + y);
 			
 			System.out.println("Sum of salary of people whose name starts with 'M': " + sum);
 			
